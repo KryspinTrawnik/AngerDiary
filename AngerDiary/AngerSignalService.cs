1ff613806ee6b77061d6a49bf5f3846292111903
Kryspin Trawnik <ktrawnik@gmail.com>
2020-09-20 18:17:00 +0100

    commit

--- AngerDiary/AngerSignalService.cs
+++ AngerDiary/AngerSignalService.cs
@@ -5,6 +5,7 @@ namespace AngerDiary
     class AngerSignalService
     {
         private List<AngerSignal> angerSignals;
+        
         public AngerSignalService()
         { 
         
@@ -14,17 +15,17 @@ namespace AngerDiary
         {
             this.angerSignals = angerSignals;
         }
-        public void AddNewSignal(int Signalid, string Signalname, bool hasbeenused)
+        public void AddNewSignal(int signalId, string signalName, bool hasBeenUsed)
         {
-            AngerSignal angerSignal = new AngerSignal(Signalid, Signalname, hasbeenused);
+            AngerSignal angerSignal = new AngerSignal(signalId, signalName, hasBeenUsed);
             angerSignals.Add(angerSignal);
         }
-        public List<AngerSignal> AddNotUsedSignal(bool hasbeenused)
+        public List<AngerSignal> AddNotUsedSignal(bool hasBeenUsed)
         {
             List<AngerSignal> result = new List<AngerSignal>();
             foreach(var signal in angerSignals)
             {
-                if (hasbeenused == false)
+                if (signal.Hasbeenused == false)
                 {
                     result.Add(signal);
                 }
--- AngerDiary/EventService.cs
+++ AngerDiary/EventService.cs
@@ -14,19 +14,6 @@ namespace AngerDiary
 
         }
         
-        private static AngerSignalService Initialize(AngerSignalService angersignalService)
-        {
-            angersignalService.AddNewSignal(1, "Raised voice", false);
-            angersignalService.AddNewSignal(2, "Headaches", false);
-            angersignalService.AddNewSignal(3, "Stomachaches", false);
-            angersignalService.AddNewSignal(4, "Increased heart rate", false);
-            angersignalService.AddNewSignal(5, "Raised blood pressure", false);
-            angersignalService.AddNewSignal(6, "Clenching your jaws or grinding your teeth", false);
-            angersignalService.AddNewSignal(7, "Clinched fists", false);
-            angersignalService.AddNewSignal(8, "Sweating, especially your palms", false);
-            angersignalService.AddNewSignal(9, "Feeling hot in the neck/face", false);
-
-            return angersignalService;
-        }
+       
     }
 }
