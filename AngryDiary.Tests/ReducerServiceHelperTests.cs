8f57866a19c24f5b68167d7ae6c56ab934408400
Kryspin Trawnik <ktrawnik@gmail.com>
2021-02-09 22:16:55 +0000

    ReducerServiceHelperTests

---
+++ AngryDiary.Tests/ReducerServiceHelperTests.cs
@@ -0,0 +1,42 @@
+﻿using AngerDiary.Domain.Entity;
+using Xunit;
+using FluentAssertions;
+using AngerDiary.App.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngryDiary.Tests
+{
+    public class ReducerServiceHelperTests
+    {
+        [Fact]
+        public void PickFromInitialList_isNotNull()
+        {
+            //Arrange
+            var result = new List<Reducer>();
+            //Act
+            result = result.PickFromInitialList();
+            //Assert
+            result.Should().NotBeNull();
+        }
+        [Theory]
+        [InlineData(1, "Counting backward", false)]
+        [InlineData(2, "Deep breathing", false)]
+        [InlineData(3, "Thinking ahead (if - consequences)", false)]
+        [InlineData(4, "Positive visualisation", false)]
+        public void PickFromInitialList_ReturnsCorrectData(int id, string name, bool hasBeenUsed)
+        {
+            //Arrange
+            var result = new List<Reducer>();
+            
+            //Act
+            result = result.PickFromInitialList();
+            //Assert
+            result[id].Name.Should().Be(name);
+            result[id].HasBeenUsed.Should().Be(hasBeenUsed);
+        }
+    }
+}
