37d9762e284674822919b84e5ed62d22b2e1e71f
Kryspin Trawnik <ktrawnik@gmail.com>
2021-02-10 06:55:40 +0000

    StageToImproveServiceTests

---
+++ AngryDiary.Tests/StageToImproveServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using FluentAssertions;
+using AngerDiary.App.Concrete;
+using AngerDiary.Domain.Entity;
+
+
+namespace AngryDiary.Tests
+{
+    public class StageToImproveServiceTests
+    {
+        [Fact]
+        public void StageToImprove_IsNotNull()
+        {
+            //Act
+            var stageService = new StageService();
+            var stageToImproveService = new StageToImproveService();
+            var result = new Stage();
+            //Arrange
+            result = stageToImproveService.StageToImprove(stageService.Items);
+            //Assert
+            result.Should().NotBeNull();
+        }
+        [Fact]
+        public void StageToImprove_ReturnsCorrectData()
+        {
+            //Act
+            var stageService = new StageService();
+            var stageToImproveService = new StageToImproveService();
+            var result = new Stage();
+            //Arrange
+            result = stageToImproveService.StageToImprove(stageService.Items);
+            //Assert
+            result.Should().Be(stageService.Items[2]);
+        }
+    }
+}
